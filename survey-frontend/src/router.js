7c76e05ddf5ecf028c5fa5804392f672116e6f2b
bosterm <bostermeierm@gmail.com>
2021-10-17 16:03:52 -0500

    Create router.js

---
+++ survey-frontend/src/router.js
@@ -0,0 +1,26 @@
+import Vue from "vue";
+import Router from "vue-router";
+
+Vue.use(Router);
+
+export default new Router({
+//  mode: "history",
+  routes: [
+    {
+      path: "/",
+      alias: "/survey-responses",
+      name: "survey-responses",
+      component: () => import("./components/SurveyResponses")
+    },
+    {
+      path: "/survey-responses/:id",
+      name: "survey-response-details",
+      component: () => import("./components/SurveyResponse")
+    },
+    {
+      path: "/add",
+      name: "add",
+      component: () => import("./components/AddSurveyResponse")
+    }
+  ]
+});

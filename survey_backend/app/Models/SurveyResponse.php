3f6c790c2330dded1a3c360851e30321ea622df2
bosterm <bostermeierm@gmail.com>
2021-10-18 21:31:37 -0500

    Fix surveyresponse.php, now the submissions work!

--- survey_backend/app/Models/SurveyResponse.php
+++ survey_backend/app/Models/SurveyResponse.php
@@ -8,6 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class SurveyResponse extends Model
 {
     use HasFactory;
+    
+    protected $fillable = ['response'];
 
     public static function generate()
     {

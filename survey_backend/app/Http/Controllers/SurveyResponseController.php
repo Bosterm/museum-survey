24b18b5ceb463970f721e45d6c54761e1625ecb8
bosterm <bostermeierm@gmail.com>
2021-10-18 20:08:53 -0500

    Add rule to surveyresponsecontroller

--- survey_backend/app/Http/Controllers/SurveyResponseController.php
+++ survey_backend/app/Http/Controllers/SurveyResponseController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\SurveyResponse;
+use Illuminate\Validation\Rule;
 
 class SurveyResponseController extends Controller
 {

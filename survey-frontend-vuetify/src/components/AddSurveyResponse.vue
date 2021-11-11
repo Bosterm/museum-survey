844880b34d68aac655cd41ec325e0e940b1760b2
bosterm <bostermeierm@gmail.com>
2021-11-10 20:48:49 -0600

    Create AddSurveyResponse.vue

---
+++ survey-frontend-vuetify/src/components/AddSurveyResponse.vue
@@ -0,0 +1,18 @@
+<template>
+    
+</template>
+<script>
+  export default {
+    name: 'add-survey-response',
+
+    data() {
+    return {
+        surveyResponse: {
+            id: null,
+            response:''
+        },
+        submitted: false
+        };
+    },
+  }
+</script>

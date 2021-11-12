4e33aaf30f004f4e937da0831da206af881dd34a
bosterm <bostermeierm@gmail.com>
2021-11-11 21:07:03 -0600

    Remove loading stuff, not needed

--- survey-frontend-vuetify/src/components/AddSurveyResponse.vue
+++ survey-frontend-vuetify/src/components/AddSurveyResponse.vue
@@ -1,12 +1,5 @@
 <template>
-  <v-card :loading="loading" class="mx-auto my-12" max-width="374">
-    <template slot="progress">
-      <v-progress-linear
-        color="deep-purple"
-        height="10"
-        indeterminate
-      ></v-progress-linear>
-    </template>
+  <v-card class="mx-auto my-12" max-width="374">
 
     <v-img :src="require('../assets/Dress.png')"></v-img>
 

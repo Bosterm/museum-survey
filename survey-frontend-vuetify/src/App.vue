eac396142923d1b25c3f7f665d6f8a6b3a9897c6
bosterm <bostermeierm@gmail.com>
2021-11-21 12:14:23 -0600

    Load add submission at /add directory

--- survey-frontend-vuetify/src/App.vue
+++ survey-frontend-vuetify/src/App.vue
@@ -1,23 +1,13 @@
 <template>
   <v-app>
     <v-main>
-      <AddSurveyResponse/>
+      <router-view />
     </v-main>
   </v-app>
 </template>
 
 <script>
-import AddSurveyResponse from './components/AddSurveyResponse';
-
 export default {
   name: 'App',
-
-  components: {
-    AddSurveyResponse,
-  },
-
-  data: () => ({
-    //
-  }),
 };
 </script>
---
+++ survey-frontend-vuetify/src/router.js
@@ -0,0 +1,21 @@
+import Vue from "vue";
+import Router from "vue-router";
+
+Vue.use(Router);
+
+export default new Router({
+//  mode: "history",
+  routes: [
+    {
+      path: "/",
+      alias: "/survey-responses",
+      name: "survey-responses",
+      component: () => import("./components/SurveyResponsesList")
+    },
+    {
+      path: "/add",
+      name: "add",
+      component: () => import("./components/AddSurveyResponse")
+    }
+  ]
+});

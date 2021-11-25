27e7ba234409f81e89743e561ea382c0c1058f0f
bosterm <bostermeierm@gmail.com>
2021-11-24 18:37:06 -0600

    Started making chart work with dress data

--- survey-frontend-vuetify/src/components/Chart.vue
+++ survey-frontend-vuetify/src/components/Chart.vue
@@ -18,14 +18,28 @@ export default {
         chart: {
           id: "vuechart-example",
         },
-        xaxis: {
-          categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998],
+        plotOptions: {
+          bar: {
+            distributed: true,
+          },
         },
+        xaxis: {
+          categories: [
+            "Blue and Black",
+            "White and Gold",
+            "Blue and Brown",
+            "Something Else",
+          ],
+        },
+        colors: ["#546E7A", "#33b2df", "#d4526e", "#13d8aa"],
+      },
+      toolbar: {
+          show:false;
       },
       series: [
         {
           name: "series-1",
-          data: [30, 40, 35, 50, 49, 60, 70, 91],
+          data: [13, 10, 5, 4],
         },
       ],
     };

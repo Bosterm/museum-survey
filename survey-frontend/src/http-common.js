c8863e2cf20341fd86217196b89544c3c7a7cbdc
bosterm <bostermeierm@gmail.com>
2022-06-25 20:58:56 -0500

    Update http-common.js
    Change backend location on server

--- survey-frontend/src/http-common.js
+++ survey-frontend/src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://dress-api.benostermeier.com:8080/api",
+  baseURL: "https://dress.benostermeier.com:8080/api",
   headers: {
     "Content-type": "application/json"
   }

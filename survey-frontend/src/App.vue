b37aaa6432404d5644881dde1a2c3c561abdcbbf
bosterm <bostermeierm@gmail.com>
2022-09-09 14:57:05 -0500

    Add new bottom nav component on main

--- survey-frontend/src/App.vue
+++ survey-frontend/src/App.vue
@@ -2,12 +2,18 @@
   <v-app>
     <v-main class="grey">
       <router-view />
+      <bottom-nav />
     </v-main>
   </v-app>
 </template>
 
 <script>
+import bottomNav from './components/BottomNav.vue'
+
 export default {
-  name: 'App',
+    name: 'App',
+    components: {
+    bottomNav
+  }
 };
 </script>

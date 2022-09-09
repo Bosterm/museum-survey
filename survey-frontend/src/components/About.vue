83ef1abcf667e3766941d382664f128c4ac90007
bosterm <bostermeierm@gmail.com>
2022-09-09 15:50:48 -0500

    Create About.vue

---
+++ survey-frontend/src/components/About.vue
@@ -0,0 +1,21 @@
+<template>
+    <v-container class="fill-height">
+      <v-row no-gutters class="align-center">
+        <v-col>
+          <v-card align-self="center" class="pa-2 mx-auto" max-width="700">
+            <v-card-title class="justify-center"
+              ><h5 class="text-h5 text-center">
+                About
+              </h5></v-card-title
+            >
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </template>
+
+<script>
+export default {
+    name: 'about',
+};
+</script>
